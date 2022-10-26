cf7e79a5463e73c3f00d58b48acc3c3faca2ec60
tony-cannibal <91645702+tony-cannibal@users.noreply.github.com>
2022-10-26 06:54:21 -0500

    Create install_arch_basic.sh

---
+++ install_arch_basic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# TimeZone
+ln -sf /usr/share/zoneinfo/America/Mexico_City /etc/localtime
+hwclock --systohc
+
+# Localization
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf\
+
+# Network Specific
+echo "cannibal" >> /etc/hostname
+echo "127.0.0.1        localhost" >> /etc/hosts
+echo "::1              localhost" >> /etc/hosts
+echo "127.0.1.1        myhostname.localdomain  myhostname" >> /etc/hosts
+
+echo root:260593 | chpasswd
+
+pacman -Sy --noconfirm grub efibootmgr networkmanager mtools dosfstools xdg-user-dirs \
+  xdg-utils base-devel nfs-utils inetutils dnsutils alsa-utils pulseaudio alacritty \
+  gvfs gvfs-mtp os-prober terminus-font xorg xorg-xinit bspwm sxhkd lightdm \
+  lightdm-gtk-greeter lightdm-gtk-greeter-settings pcmanfm neovim python-neovim \
+  wget curl git unzip tar gzip npm lxsession udisks2 udiskie feh xsel exa bat \
+  ueberzug numlockx ark p7zip unrar unarchiver lzop lrzip file-roller
+
+# Grub
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+sed -i 's/GRUB_GFXMODE=.*/GRUB_GFXMODE=1440x900x32,auto/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager 
+
+useradd luis -mG wheel
+echo luis:310013 | chpasswd
